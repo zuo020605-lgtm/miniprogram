e3e57a6bc733268dd2053225b9699d0cb8e0b96e
unknown <zuo020605@gmail.com>
2026-04-09 15:39:01 +0800

    修改了

---
+++ components/skeleton/index.js
@@ -0,0 +1,18 @@
+Component({
+  properties: {
+    type: {
+      type: String,
+      value: 'list'
+    },
+    count: {
+      type: Number,
+      value: 3
+    }
+  },
+  data: {
+    // 组件内部数据
+  },
+  methods: {
+    // 组件方法
+  }
+})
